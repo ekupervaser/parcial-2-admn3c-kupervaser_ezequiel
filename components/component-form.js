a91e2da657b03f6c96c18711a1159eb1a7b19622
Ezequiel Kupervaser <ezequiel@kyas.com.ar>
2023-05-13 17:11:43 -0300

    Guardado de información en Local Storage

--- components/component-form.js
+++ components/component-form.js
@@ -1,51 +1,99 @@
 Vue.component('component-form', {
     data: function() {
-        return {form: "Soy un form desde un componente"}
+        return {
+            nombre: '',
+            especie: '',
+            raza: '',
+            edad: '',
+            sexo: '',
+            color: '',
+            tamanio: '',
+            nombreDueno: '',
+            emailDueno: '',
+            telefonoDueno: '',
+            infoAdicional: '',
+        }
+    },
+
+    methods: {
+        submitForm: function (event) {
+            event.preventDefault();
+            
+            let newAnimal = {
+                animalName: this.nombre,
+                especie: this.especie,
+            }
+           
+
+            this.setNewAnimal(newAnimal)
+        },
+
+        setNewAnimal: function (newAnimal) {
+            // obtener de localstorage si existe o no listaAnimales
+           let oldListAnimals = localStorage.getItem("listaAnimales")
+          // Si tiene informacion
+           if(oldListAnimals) {
+            // transforma la lista en formato string en array 
+           let arrayOldListAnimals= JSON.parse(oldListAnimals)
+           // Inserta el nuevo animal en la lista vieja de animales
+           arrayOldListAnimals.push(newAnimal)
+            // Lo vuelve a seter en localstorage con el nuevo animal
+            localStorage.setItem("listaAnimales",  JSON.stringify(arrayOldListAnimals))
+        }else {
+            // Si no existe en localstorage la key de listaAnimales la crea y le agrega el nuevo animal en un array transformando en string
+            localStorage.setItem("listaAnimales",  JSON.stringify([newAnimal]))
+        }
+
+        }
     },
 
     template: 
-    `<form action="">
+    `<div>
+    <p>{{nombre}}</p>
+    <form action="">
     <label for="animalName">Nombre del animal:</label>
-    <input type="text" id="animalName" name="animalName" required />
+    <input type="text" id="animalName" name="animalName" v-model="nombre" />
 
     <label for="species">Especie:</label>
-    <input type="text" id="species" name="species" required />
+    <input type="text" id="species" name="species"  v-model="especie"/>
 
     <label for="breed">Raza:</label>
-    <input type="text" id="breed" name="breed" />
+    <input type="text" id="breed" name="breed" v-model="raza"/>
 
     <label for="age">Edad:</label>
-    <input type="text" id="age" name="age" />
+    <input type="text" id="age" name="age" v-model="edad"/>
 
     <label for="sex">Sexo:</label>
-    <select id="sex" name="sex">
+    <select id="sex" name="sex" v-model="sexo">
       <option value="male">Macho</option>
       <option value="female">Hembra</option>
       <option value="other">Otro</option>
     </select>
 
     <label for="color">Color o patrón:</label>
-    <input type="text" id="color" name="color" />
+    <input type="text" id="color" name="color" v-model="color"/>
 
     <label for="size">Tamaño:</label>
-    <select id="size" name="size">
+    <select id="size" name="size" v-model="tamanio">
       <option value="small">Pequeño</option>
       <option value="medium">Mediano</option>
       <option value="large">Grande</option>
     </select>
 
     <label for="ownerName">Nombre del dueño:</label>
-    <input type="text" id="ownerName" name="ownerName" required />
+    <input type="text" id="ownerName" name="ownerName"  v-model="nombreDueno"/>
 
     <label for="email">Correo electrónico del dueño:</label>
-    <input type="email" id="email" name="email" required />
+    <input type="email" id="email" name="email"  v-model="emailDueno"/>
 
     <label for="phone">Número de teléfono del dueño:</label>
-    <input type="tel" id="phone" name="phone" required />
+    <input type="tel" id="phone" name="phone"  v-model="telefonoDueno"/>
 
     <label for="additionalInfo">Información adicional:</label>
-    <textarea id="additionalInfo" name="additionalInfo"></textarea>
+    <textarea id="additionalInfo" name="additionalInfo" v-model="infoAdicional"></textarea>
 
-    <button type="submit">Registrar</button>
-  </form>`
+    <button type="submit" v-on:click="submitForm($event)">Registrar</button>
+  </form>
+  </div>
+  `
 });
