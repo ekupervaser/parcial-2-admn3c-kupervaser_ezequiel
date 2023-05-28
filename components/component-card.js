f3bae71638bcd629dd2ae31e6e5e113e3b838222
Ezequiel Kupervaser <ezequiel@kyas.com.ar>
2023-05-28 19:59:57 -0300

    Filtro capitalize

--- components/component-card.js
+++ components/component-card.js
@@ -2,27 +2,34 @@ Vue.component('component-card', {
  
 props: ["nombre", "especie", "raza", "edad", "sexo", "color", "tamanio", "nombreDueno", "emailDueno", "telefonoDueno", "infoAdicional"],
 
+filters: {
+  capitalize: function (value) {
+    if (!value) return ''
+    value = value.toString()
+    return value.charAt(0).toUpperCase() + value.slice(1)
+  }
+},
+
     template: 
     `<li class="card">
     <h2>Información de la mascota</h2>
     <ul>
-      <li><strong>Nombre:</strong> <span>{{nombre}}</span></li>
-      <li><strong>Especie:</strong> <span>{{especie}}</span></li>
-      <li><strong>Raza:</strong> <span>{{raza}}</span></li>
-      <li><strong>Edad:</strong> <span>{{edad}}</span></li>
+      <li><strong>Nombre:</strong> <span>{{nombre | capitalize}}</span></li>
+      <li><strong>Especie:</strong> <span>{{especie | capitalize}}</span></li>
+      <li><strong>Raza:</strong> <span>{{raza | capitalize}}</span></li>
+      <li><strong>Edad:</strong> <span>{{edad | capitalize}}</span></li>
       <li><strong>Sexo:</strong> 
-      <img v-if="sexo=='macho'" src="../img/masculino.png" alt="Macho"> 
-      <img v-else src="../img/femenino.png" alt="Hembra"> 
-      
-      <span>{{sexo}}</span></li>
-      <li><strong>Color:</strong> <span>{{color}}</span></li>
-      <li><strong>Tamaño:</strong> <span>{{tamanio}}</span></li>
+        <img v-if="sexo=='macho'" src="../img/masculino.png" alt="Macho"> 
+        <img v-else src="../img/femenino.png" alt="Hembra"> 
+      </li>
+      <li><strong>Color:</strong> <span>{{color | capitalize}}</span></li>
+      <li><strong>Tamaño:</strong> <span>{{tamanio | capitalize}}</span></li>
     </ul>
     <h2>Información del dueño</h2>
     <ul>
-      <li><strong>Nombre del Dueño:</strong> <span>{{nombreDueno}}</span></li>
-      <li><strong>Email del Dueño:</strong> <span>{{emailDueno}}</span></li>
-      <li><strong>Teléfono del Dueño:</strong> <span>{{telefonoDueno}}</span></li>
+      <li><strong>Nombre del Dueño:</strong> <span>{{nombreDueno | capitalize}}</span></li>
+      <li><strong>Email del Dueño:</strong> <span>{{emailDueno | capitalize}}</span></li>
+      <li><strong>Teléfono del Dueño:</strong> <span>{{telefonoDueno | capitalize}}</span></li>
     </ul>
     <h2>Información adicional</h2>
     <p>{{infoAdicional}}</p>
